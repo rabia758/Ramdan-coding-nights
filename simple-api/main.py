5f2c8015c68008c2890305f1ce30254ee7f4dfaf
rabia758 <rizwanrabia758@gmail.com>
2025-03-10 11:56:12 +0500

    first commit

---
+++ simple-api/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+import random
+   
+app = FastAPI()
+
+side_hustle = [
+    "Freelancing - Start Offering Your Skill online!",
+    "Dropshipping - sell without handing inventory!",
+    "E-commerce - Start Your Own store!",
+    "Online Courses - Share Your Knowledge and earn money!",
+    "Blogging - create content and earn through ads and sponsorship!",
+    "youtube channel - Monetize vedeios through ads and sponsorship!"
+]
+
+money_quotes = [
+    "Too many people spend money they earned..to buy things they don't want..to impress people that they don't like. --Will Rogers",
+    "A wise person should have money in their head, but not in their heart. --Jonathan Swift",
+    "Wealth consists not in having great possessions, but in having few wants. --Epictetus",
+    "Money often costs too much. --Ralph Waldo Emerson",
+    "Everyday is a bank account, and time is our currency. No one is rich, no one is poor, we've got 24 hours each. --Christopher Rice",
+    "It's how you deal with failure that determines how you achieve success. --David Feherty"
+    ]
+
+@app.get("/side_hustles")
+def get_side_hustles(apikey: str):
+    """Return a random side Hustle idea"""
+    if apikey != "1234567890":
+        return {"error": "Invalid Api key!"}
+    return {"side_hustle": random.choice(side_hustle)}
+
+@app.get("/money_quotes")
+def get_money_quotes(apikey: str):
+    """Return a random money quote"""
+    if apikey != "1234567890":
+        return {"error": "Invalid Api key!"}
+    return{"moneyquote" : random.choice(money_quotes)}
+
+
+
+
